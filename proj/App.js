4b01ea6b6c001c7d21689db082e2653dc2dcd20f
alan <alanyc@uchicago.edu>
2019-03-13 00:29:17 -0500

    add alarm fxn

--- proj/App.js
+++ proj/App.js
@@ -15,7 +15,10 @@ export default class Sensors extends React.Component {
         this.state = {
             status: false, // data capture on/off toggle
             motionData: [], // array of data points captured from phone sensors
-            alarm: new Date()
+            alarm_time: new Date(),
+            alarm_armed: false,
+            alarm_ringing: false,
+            alarm_offed: false,
         };
 
         this._setDate = this._setDate.bind(this)
@@ -49,6 +52,22 @@ export default class Sensors extends React.Component {
             // append most recent motion data to past data
             let tmp = this.state.motionData.concat(motionData);
             this.setState({motionData: tmp});
+
+            if (this.state.alarm_armed) {
+                let cur_date = new Date();
+                const cur_hrs = cur_date.getUTCHours();
+                const cur_min = cur_date.getUTCMinutes();
+                let alm_date = this.state.alarm_time;
+                const alm_hrs = alm_date.getUTCHours();
+                const alm_min = alm_date.getUTCMinutes();
+
+                if ((cur_hrs === alm_hrs) && (cur_min === alm_min)) {
+                    if (!this.state.alarm_offed) {
+                        console.log("alarm ringing");
+                        this._alarmRing();
+                    }
+                }
+            }
         })
     };
 
@@ -56,42 +75,71 @@ export default class Sensors extends React.Component {
         this.subscription && this.subscription.remove();
         this.subscription = null;
 
-        console.log(JSON.stringify(this.state.motionData))
+        console.log(JSON.stringify(this.state.motionData));
 
-        this.setState({motionData: []})
+        this.setState({motionData: []});
+    };
+
+    _alarmOn = () => {
+        this.setState({alarm_armed: true});
+        this.setState({alarm_offed: false});
+        console.log("on");
+    };
+
+    _alarmOff = () => {
+        this.setState({alarm_armed: false});
+        this.setState({alarm_ringing: false});
+        console.log("off");
     };
 
     _setDate(newDate) {
-        this.setState({alarm: newDate})
-    }
+        this.setState({alarm_time: newDate});
+    };
+
+    _alarmRing = () => {
+        this.setState({alarm_ringing: true});
+    };
+
+    _alarmRingOff = () => {
+        this.setState({alarm_ringing: false});
+        this.setState({alarm_offed: true});
+    };
 
     render() {
+        console.log("ALARM: ", this.state.alarm_ringing);
         return (
             <View style={styles.container}>
                 <DatePickerIOS
-                    date={this.state.alarm}
+                    date={this.state.alarm_time}
                     onDateChange={this._setDate}
                     mode={'time'}
                 />
                 <TouchableOpacity onPress={this._toggle}>
-                    <Text>Toggle</Text>
+                    <Text>Toggle{'\n'}</Text>
                 </TouchableOpacity>
+
+                <TouchableOpacity onPress={this._alarmOn}>
+                    <Text>Alarm On{'\n'}</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={this._alarmOff}>
+                    <Text>Alarm Off{'\n'}</Text>
+                </TouchableOpacity>
+
+                {this.state.alarm_ringing === true ?
+                    <TouchableOpacity onPress={this._alarmRingOff}>
+                        <Text>Ringer Off{'\n'}</Text>
+                    </TouchableOpacity>
+                    :
+                    <Text></Text>}
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center'
-  },
-})
-
-// //processes cumulative dataset to determine whether user is exercising or not, every second
-// processdata() {
-//   console.log("i'm processing!");
-//   //Calculate standard deviation of accl and gyro dataset
-//   //data SCIENCe! train on datasets where we're still vs not still
-//
-// }
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+});
